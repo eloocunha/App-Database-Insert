72483dbb3feb30739d4e110db78aae07cb53f831
Eloisa <elo.c.antoniassi@gmail.com>
2024-10-30 01:55:05 -0300

    Insert app database

---
+++ AppDatabase/app/src/main/java/mobile/fatec/appdatabase/MainActivity.kt
@@ -0,0 +1,188 @@
+package mobile.fatec.appdatabase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import edu.example.appauladsm.roomDB.Pessoa
+import edu.example.appauladsm.roomDB.PessoaDataBase
+import edu.example.appauladsm.ui.theme.AppAulaDSMTheme
+import edu.example.appauladsm.viewModel.PessoaViewModel
+import edu.example.appauladsm.viewModel.Repository
+
+class MainActivity : ComponentActivity() {
+    private val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            PessoaDataBase::class.java,
+            "pessoas.db"
+        ).build()
+    }
+
+    private val viewModel by viewModels<PessoaViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return PessoaViewModel(Repository(db)) as T
+                }
+            }
+        }
+    )
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppAulaDSMTheme {
+                @OptIn(ExperimentalMaterial3Api::class)
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    App(viewModel, this)
+
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+
+fun App(viewModel: PessoaViewModel, mainActivity: MainActivity){
+
+    var nome by remember{
+        mutableStateOf("")
+    }
+    var telefone by remember{
+        mutableStateOf("")
+    }
+    val pessoa = Pessoa(
+        nome,
+        telefone
+    )
+
+    val pessoaList by remember{
+        mutableStateOf(listOf<Pessoa>())
+    }
+
+
+
+    Column(
+        Modifier
+            .background(Color.Black)
+            .fillMaxHeight()
+            .fillMaxWidth()
+    )
+    {
+        Row(Modifier.padding(20.dp)) {}
+        Row(
+            Modifier.fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            Text(
+                "App Database",
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                color = Color.White
+            )
+        }
+        Row(Modifier.padding(20.dp)) {}
+
+        Row(Modifier.fillMaxWidth(), Arrangement.Center) {
+            Column(Modifier.fillMaxWidth(0.2f)) {
+                Text(
+                    "Nome: ",
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+            }
+            Column {
+                TextField(
+                    value = nome,
+                    onValueChange = { nome = it },
+                    label = { },
+                    modifier = Modifier.height(56.dp)
+                )
+            }
+        }
+
+
+        Row(Modifier.padding(20.dp)) {}
+
+        Row(Modifier.fillMaxWidth(), Arrangement.Center)
+        {
+            Column(Modifier.fillMaxWidth(0.2f)) {
+                Text(
+                    "Telefone: ",
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+            }
+            Column() {
+                TextField(
+                    value = telefone,
+                    onValueChange = {telefone = it},
+                    label = { },
+                    modifier = Modifier.height(56.dp)
+                )
+            }
+        }
+        Row(
+            Modifier
+                .padding(20.dp)
+        ) {
+        }
+
+        Row(Modifier.fillMaxWidth(), Arrangement.Center) {
+            Button(
+                onClick = {
+                    viewModel.upsertPessoa(pessoa)
+                    nome = ""
+                    telefone = ""
+                }
+            ) {
+                Text(
+                    "Cadastrar",
+                    fontFamily = FontFamily.SansSerif,
+                    fontSize = 16.sp,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
